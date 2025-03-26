f295cfaef8ed525aff80b774e39c6e6bd3e3e549
Marquis Sampson <marquis4484@gmail.com>
2025-03-25 21:59:39 -0400

    colors added

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -19,6 +19,8 @@ const config = {
     },
     extend: {
       colors: {
+        primary: "var(--color-primary)",
+        secondary: "var(--color-secondary)",
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
         ring: "hsl(var(--ring))",
